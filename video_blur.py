c45852a8586fce2568d73cf2a60700d2051b8520
arif124713 <arifhussain124713@gmail.com>
2025-07-21 01:35:20 +0600

    face blur

---
+++ video_blur.py
@@ -0,0 +1,31 @@
+import cv2
+import mediapipe as mp
+
+
+
+cap= cv2.VideoCapture(0)
+ret,frame= cap.read()
+while ret:
+    ret, frame = cap.read()
+    rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    face_detection = mp.solutions.face_detection
+    H, W, _ = frame.shape
+
+    with face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+        out = face_detection.process(rgb_image)
+        if out.detections is not None:
+            for detection in out.detections:
+                location_data = detection.location_data
+                bbox = location_data.relative_bounding_box
+                x1, y1, w, h = bbox.xmin, bbox.ymin, bbox.width, bbox.height
+                x1 = int(x1 * W)
+                y1 = int(y1 * H)
+                w = int(w * W)
+                h = int(h * H)
+            frame[y1:y1 + h, x1:x1 + w, :] = cv2.blur(frame[y1:y1 + h, x1:x1 + w, :], ksize=(30, 30))
+
+    cv2.imshow('frame', frame)
+    cv2.waitKey(25)
+
+cap.release()
+cv2.destroyAllWindows()
